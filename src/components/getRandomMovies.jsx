274196468c2c77a8abe5d7b7a3fe5748754d3c5a
Mohammed Faheem <faasu2327@gmail.com>
2025-06-10 14:35:14 +0530

    Made default for getRandomMovies

--- src/components/getRandomMovies.jsx
+++ src/components/getRandomMovies.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-export function getRandomMovies(movieList, count = 20) {
+export default function getRandomMovies(movieList, count = 20) {
     const shuffled = [...movieList]; 
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
